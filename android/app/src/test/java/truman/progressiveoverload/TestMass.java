9eccc8e0db73d5f71937b6fbe4e60e2f8d7fd1b9
TrumanYen <yen.truman@gmail.com>
2024-03-31 19:19:15 -0600

    Upgraded to junit5

--- android/app/src/test/java/truman/progressiveoverload/TestMass.java
+++ android/app/src/test/java/truman/progressiveoverload/TestMass.java
@@ -1,8 +1,11 @@
 package truman.progressiveoverload;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.concurrent.ThreadLocalRandom;
 
