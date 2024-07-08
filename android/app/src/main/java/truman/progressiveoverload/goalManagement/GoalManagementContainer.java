f46d3771313d74b36ae10276d78a88d56b61e3e1
TrumanYen <yen.truman@gmail.com>
2024-07-07 23:22:59 -0600

    Added custom unit goal management module

--- android/app/src/main/java/truman/progressiveoverload/goalManagement/GoalManagementContainer.java
+++ android/app/src/main/java/truman/progressiveoverload/goalManagement/GoalManagementContainer.java
@@ -3,6 +3,7 @@ package truman.progressiveoverload.goalManagement;
 
 import truman.progressiveoverload.goalManagement.api.I_GoalDataPersistenceSource;
 import truman.progressiveoverload.goalManagement.api.I_GoalManagementContainer;
+import truman.progressiveoverload.measurement.custom.TimestampedCustomValue;
 import truman.progressiveoverload.measurement.distance.TimestampedDistance;
 import truman.progressiveoverload.measurement.duration.TimestampedDuration;
 import truman.progressiveoverload.measurement.mass.TimestampedMass;
@@ -13,15 +14,18 @@ public class GoalManagementContainer implements I_GoalManagementContainer {
     private final GoalManagementModule<TimestampedDistance> distanceModule_;
     private final GoalManagementModule<TimestampedDuration> durationModule_;
     private final GoalManagementModule<TimestampedVelocity> velocityModule_;
+    private final GoalManagementModule<TimestampedCustomValue> customUnitModule_;
 
     public GoalManagementContainer(I_GoalDataPersistenceSource<TimestampedMass> persistenceSourceForMassGoals,
                                    I_GoalDataPersistenceSource<TimestampedDistance> persistenceSourceForDistanceGoals,
                                    I_GoalDataPersistenceSource<TimestampedDuration> persistenceSourceForDurationGoals,
-                                   I_GoalDataPersistenceSource<TimestampedVelocity> persistenceSourceForVelocityGoals) {
+                                   I_GoalDataPersistenceSource<TimestampedVelocity> persistenceSourceForVelocityGoals,
+                                   I_GoalDataPersistenceSource<TimestampedCustomValue> persistenceSourceForCustomGoals) {
         massModule_ = new GoalManagementModule<>(persistenceSourceForMassGoals);
         distanceModule_ = new GoalManagementModule<>(persistenceSourceForDistanceGoals);
         durationModule_ = new GoalManagementModule<>(persistenceSourceForDurationGoals);
         velocityModule_ = new GoalManagementModule<>(persistenceSourceForVelocityGoals);
+        customUnitModule_ = new GoalManagementModule<>(persistenceSourceForCustomGoals);
     }
 
     @Override
@@ -42,6 +46,11 @@ public class GoalManagementContainer implements I_GoalManagementContainer {
     @Override
     public GoalManagementModule<TimestampedVelocity> velocityModule() {
         return velocityModule_;
+    }
+
+    @Override
+    public GoalManagementModule<TimestampedCustomValue> customUnitModule() {
+        return customUnitModule_;
     }
 
 }
--- android/app/src/main/java/truman/progressiveoverload/goalManagement/api/I_GoalManagementContainer.java
+++ android/app/src/main/java/truman/progressiveoverload/goalManagement/api/I_GoalManagementContainer.java
@@ -1,6 +1,7 @@
 package truman.progressiveoverload.goalManagement.api;
 
 import truman.progressiveoverload.goalManagement.GoalManagementModule;
+import truman.progressiveoverload.measurement.custom.TimestampedCustomValue;
 import truman.progressiveoverload.measurement.distance.TimestampedDistance;
 import truman.progressiveoverload.measurement.duration.TimestampedDuration;
 import truman.progressiveoverload.measurement.mass.TimestampedMass;
@@ -14,4 +15,6 @@ public interface I_GoalManagementContainer {
     GoalManagementModule<TimestampedDuration> durationModule();
 
     GoalManagementModule<TimestampedVelocity> velocityModule();
+
+    GoalManagementModule<TimestampedCustomValue> customUnitModule();
 }
