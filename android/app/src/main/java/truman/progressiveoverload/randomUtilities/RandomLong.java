288eb6965e83a4bfe5903858180909698e72a342
TrumanYen <yen.truman@gmail.com>
2024-07-28 18:42:39 -0600

    Add ability to specify requirement lambda
    in RandomLong, so that users of the class can force RandomLong regenerate Longs forever until it meets some requirement.  The additional benefit is that is provided in the constructor, so it can be used to specify requirements when RandomLong is passed into other Random generators such as RandomArrayList

--- android/app/src/main/java/truman/progressiveoverload/randomUtilities/RandomLong.java
+++ android/app/src/main/java/truman/progressiveoverload/randomUtilities/RandomLong.java
@@ -3,8 +3,27 @@ package truman.progressiveoverload.randomUtilities;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomLong implements I_RandomValueGenerator<Long> {
+    public interface RequirementLambda {
+        boolean satisfiesRequirement(Long candidate);
+    }
+
+    private final RequirementLambda requirement_;
+
+    public RandomLong(RequirementLambda requirement) {
+        requirement_ = requirement;
+    }
+
+    public RandomLong() {
+        requirement_ = (anyLong) -> true;
+    }
+
     public Long generate(Long minValue, Long maxValue) {
-        return ThreadLocalRandom.current().nextLong(minValue, maxValue);
+        long randomLong = ThreadLocalRandom.current().nextLong(minValue, maxValue);
+        while (!requirement_.satisfiesRequirement(randomLong)) {
+            randomLong = ThreadLocalRandom.current().nextLong(minValue, maxValue);
+        }
+        return randomLong;
+
     }
 
     public Long generate() {
